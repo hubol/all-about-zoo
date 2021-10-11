9bb8b5feb196b59617e9177b85bb7881843b9f60
Hubol <hubol.gordon@gmail.com>
2021-10-10 23:56:09 -0500

    webcam works

--- src/igua/game.ts
+++ src/igua/game.ts
@@ -1,14 +1,20 @@
 import {Container} from "pixi.js";
+import {BaseTexture, MIPMAP_MODES, Sprite, Texture, VideoResource } from "pixi.js-legacy";
 import {AsshatTicker} from "../utils/asshatTicker";
 import {advanceKeyListener, startKeyListener} from "../utils/browser/key";
-import {AsshatApplication} from "../utils/pixi/createApplication";
-
-export let game: ReturnType<typeof createGame>;
+import {createApplication} from "../utils/pixi/createApplication";
+import {upscaleGameCanvas} from "./upscaleGameCanvas";
+import {environment} from "./environment";
+import {make2dCanvasSink} from "../utils/browser/make2dCanvasSink";
 
 export let scene: Container;
 
-function createGame(application: AsshatApplication)
+export async function createGame()
 {
+    const mediaSprite = await makeMediaSprite();
+    const application = createApplication({width: mediaSprite.width, height: mediaSprite.height, targetFps: 60, showCursor: false});
+    upscaleGameCanvas(addGameCanvasToDocument(application.canvasElement));
+
     application.ticker.start();
     application.stage.withTicker(new AsshatTicker());
     scene = application.stage;
@@ -19,4 +25,39 @@ function createGame(application: AsshatApplication)
         advanceKeyListener();
         scene.ticker.update();
     });
+
+    mediaSprite.anchor.x = 1;
+    mediaSprite.scale.x *= -1;
+    scene.addChild(mediaSprite);
+}
+
+async function makeMediaSprite() {
+    const element = await makeUserMediaVideoElement();
+    const res = new VideoResource(element);
+    const baseTexture = new BaseTexture(res, { mipmap: MIPMAP_MODES.OFF });
+    const mediaTexture = new Texture(baseTexture);
+    return new Sprite(mediaTexture);
+}
+
+async function makeUserMediaVideoElement() {
+    const media = await navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: 'user' } });
+    const videoElement = document.createElement('video');
+    videoElement.srcObject = media;
+    // https://stackoverflow.com/a/54678952
+    videoElement.setAttribute('autoplay', '');
+    videoElement.setAttribute('muted', '');
+    videoElement.setAttribute('playsinline', '');
+    await videoElement.play();
+    return videoElement;
+}
+
+function addGameCanvasToDocument(element: HTMLCanvasElement)
+{
+    if (environment.isSafari)
+        element = make2dCanvasSink(element);
+
+    element.id = "gameCanvas";
+    document.body.appendChild(element);
+
+    return element;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 import * as PIXI from "pixi.js";
-import {createApplication} from "./utils/pixi/createApplication";
-import {upscaleGameCanvas} from "./igua/upscaleGameCanvas";
 import {handleIguaPromiseRejection} from "./utils/rejection";
-import {make2dCanvasSink} from "./utils/browser/make2dCanvasSink";
 import {handlePromiseCancellation} from "pissant";
 import {environment} from "./igua/environment";
 
 (PIXI.settings as any).ROUND_PIXELS = true;
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
-const application = createApplication({width: 256, height: 256, targetFps: 60, showCursor: false});
 
 async function initialize()
 {
-    upscaleGameCanvas(addGameCanvasToDocument(application.canvasElement));
     // @ts-ignore
     require("./utils/extensions/**/*.*");
     // @ts-ignore
-    require("./igua/game");
+    setTimeout(require("./igua/game").createGame);
 }
 
 if (environment.isProduction && !environment.isElectron)
@@ -37,15 +32,4 @@ function createStartGameButtonElement()
         setTimeout(initialize);
     };
     return buttonElement;
-}
-
-function addGameCanvasToDocument(element: HTMLCanvasElement)
-{
-    if (environment.isSafari)
-        element = make2dCanvasSink(element);
-
-    element.id = "gameCanvas";
-    document.body.appendChild(element);
-
-    return element;
 }
